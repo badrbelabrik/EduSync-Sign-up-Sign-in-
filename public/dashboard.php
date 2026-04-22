37543ab4399912822d8ff570a4c40c43ea4f1117
badrbelabrik1 <badr.belabrik@gmail.com>
2026-04-22 23:13:40 +0100

    show authenticated user information on dashboard using session

--- public/dashboard.php
+++ public/dashboard.php
@@ -5,6 +5,7 @@ if (!isset($_SESSION['userid'])) {
     exit();
 }
 ?>
+<?php include '../includes/header.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,10 +15,10 @@ if (!isset($_SESSION['userid'])) {
 </head>
 <body>
     <main class="min-h-screen flex flex-col justify-center items-center">
+            <h1 class="text-blue-500 text-2xl font-bold">WELCOME TO THE DASHBOARD</h1>
             <p>user id: <?php echo $_SESSION['userid']; ?></p>
             <p>username: <?php echo $_SESSION['firstname'] .' '. $_SESSION['lastname']; ?></p>
             <p>email: <?php echo $_SESSION['email']; ?></p>
     
     </main>
-</body>
-</html>
+<?php include '../includes/footer.php'; ?>
